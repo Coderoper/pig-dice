374fe1437b3af6cd06598d04541e93e79c6960d7
Austin Roberts, Jimmy McNamara and Kristi Hwang <austin@>
2018-04-04 15:22:01 -0700

    add hold button, add event listener for hold button, add function endGame, add round score to total score

--- js/scripts.js
+++ js/scripts.js
@@ -2,53 +2,61 @@
 var roll;
 var roundTotal=0;
 var gameTotal=0;
-
+var round=1;
+function endGame () {
+  if (gameTotal >= 100) {
+    alert("CONGRATULATIONS!!!!!!!!!!!!!!! you have won the game.");
+    gameTotal = 0;
+    round = 1;
+  }
+}
 function rollDice() {
 roll = Math.floor((Math.random()*6) +1)
-// console.log(roll);
 };
 
 function roundScore(){
-  roundTotal += roll;
-  console.log("roll " + roll + "roundTotal " + roundTotal);
-  return roundTotal;
+  if(roll==1){
+    // $("#player-one-roll").toggle();
+    // $("#player-one-hold").toggle();
+    // $("#player-two-hold").toggle();
+    round+=1;
+    return roundTotal=0;
+  } else {
+    roundTotal += roll;
+  }
+
 
 };
 
 function gameScore(){
   gameTotal +=roundTotal;
-  console.log(gameTotal);
+  round+=1;
+  roll = 0;
+  roundTotal = 0;
   return gameTotal;
-
 };
-
-
-
-/*
-function Roll(number){
-  this.number = number;
-}
-function Round (){
-
-}
-
-
-Roll.prototype.Points function (points){
-  return Math.random+=points
-}
-*/
-
 
 
 //user logic
 $(document).ready(function(){
-  $("button").click(function(event){
+  $("div#player-one-roll").click(function(event){
     event.preventDefault();
     rollDice();
     roundScore();
+    // gameScore();
+    $("#currentRoll").text(roll);
+    $("#rollScore").text(roundTotal);
+    $("#roundNum").text(round);
+  });
+  $("div#player-one-hold").click(function(event) {
+    event.preventDefault();
     gameScore();
     $("#currentRoll").text(roll);
     $("#rollScore").text(roundTotal);
     $("#gameScore").text(gameTotal);
+    $("#roundNum").text(round);
+    endGame();
+    $("#gameScore").text(gameTotal);
+    $("#roundNum").text(round);
   })
-})
+});
