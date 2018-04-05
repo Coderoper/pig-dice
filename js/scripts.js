d994f44605a64bcd6dd3ec29af1abf4cb45893b2
Austin Roberts, Jimmy McNamara and Kristi Hwang <jimmy@>
2018-04-05 14:59:43 -0700

    add player2, add round number for both players, reset game stats once a player wins

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,136 @@
 //business logic
 var roll;
 var roundTotal=0;
-var gameTotal=0;
+var gameTotal1=0;
+var gameTotal2=0;
 var round=1;
 function endGame () {
-  if (gameTotal >= 100) {
-    alert("CONGRATULATIONS!!!!!!!!!!!!!!! you have won the game.");
-    gameTotal = 0;
+  if (gameTotal2 >= 100) {
+    gameTotal1 = 0;
+    gameTotal2 = 0;
     round = 1;
+    alert("CONGRATULATIONS!!!!!!!!!!!!!!! you have won the game.");
+
+  } else if (gameTotal1 >=100){
+    $("#player1 button.roll").toggle();
+    $("#player1 button.hold").toggle();
+    $("#player2 button.roll").toggle();
+    $("#player2 button.hold").toggle();
+    gameTotal1 = 0;
+    gameTotal2 = 0;
+    round = 1;
+    alert("CONGRATULATIONS!!!!!!!!!!!!!!! you have won the game.");
+
   }
+
 }
 function rollDice() {
 roll = Math.floor((Math.random()*6) +1)
 };
 
-function roundScore(){
+function roundScoreP1(){
   if(roll==1){
-    // $("#player-one-roll").toggle();
-    // $("#player-one-hold").toggle();
-    // $("#player-two-hold").toggle();
-    round+=1;
+    $("#player1 button.roll").toggle();
+    $("#player1 button.hold").toggle();
+    $("#player2 button.roll").toggle();
+    $("#player2 button.hold").toggle();
     return roundTotal=0;
   } else {
     roundTotal += roll;
-  }
+  };
+};
+  function roundScoreP2(){
+    if(roll==1){
+      $("#player1 button.roll").toggle();
+      $("#player1 button.hold").toggle();
+      $("#player2 button.roll").toggle();
+      $("#player2 button.hold").toggle();
+      round +=1;
+      return roundTotal=0;
+    } else {
+      roundTotal += roll;
+
+    }
 
 
 };
 
-function gameScore(){
-  gameTotal +=roundTotal;
+function gameScoreP1(){
+  $("#player1 button.roll").toggle();
+  $("#player1 button.hold").toggle();
+  $("#player2 button.roll").toggle();
+  $("#player2 button.hold").toggle();
+  gameTotal1 +=roundTotal;
+  // round+=1;
+  roll = 0;
+  roundTotal = 0;
+  return gameTotal1;
+};
+function gameScoreP2(){
+  $("#player1 button.roll").toggle();
+  $("#player1 button.hold").toggle();
+  $("#player2 button.roll").toggle();
+  $("#player2 button.hold").toggle();
+  gameTotal2 +=roundTotal;
   round+=1;
   roll = 0;
   roundTotal = 0;
-  return gameTotal;
+  return gameTotal2;
 };
 
 
 //user logic
 $(document).ready(function(){
-  $("div#player-one-roll").click(function(event){
+  $(".roundNum").text(round);
+  $("#player1 .roll").click(function(event){
+    event.preventDefault();
+
+    rollDice();
+    roundScoreP1();
+    $("#player1 .currentRoll").text(roll);
+    $("#player1 .rollScore").text(roundTotal);
+    // $("#player1 .roundNum").text(round);
+  });
+  $("#player1 .hold").click(function(event) {
+    event.preventDefault();
+    gameScoreP1();
+    $("#player1 .currentRoll").text(roll);
+    $("#player1 .rollScore").text(roundTotal);
+    $("#player1 .gameScore").text(gameTotal1);
+    // $("#player1 .roundNum").text(round);
+    endGame();
+    $("#player1 .gameScore").text(gameTotal1);
+    $("#player2 .gameScore").text(gameTotal2);
+    $(".roundNum").text(round);
+
+      // $("#player1 .roundNum").text(round);
+      // $("#player2 .roundNum").text(round);
+  })
+  $("#player2 .roll").click(function(event){
     event.preventDefault();
     rollDice();
-    roundScore();
-    // gameScore();
-    $("#currentRoll").text(roll);
-    $("#rollScore").text(roundTotal);
-    $("#roundNum").text(round);
+    roundScoreP2();
+    $("#player2 .currentRoll").text(roll);
+    $("#player2 .rollScore").text(roundTotal);
+    $(".roundNum").text(round);
+    // $("#player2 .roundNum").text(round);
+    // $("#player1 .roundNum").text(round);
   });
-  $("div#player-one-hold").click(function(event) {
+  $("#player2 .hold").click(function(event) {
     event.preventDefault();
-    gameScore();
-    $("#currentRoll").text(roll);
-    $("#rollScore").text(roundTotal);
-    $("#gameScore").text(gameTotal);
-    $("#roundNum").text(round);
+    gameScoreP2();
+    $("#player2 .currentRoll").text(roll);
+    $("#player2 .rollScore").text(roundTotal);
+    $("#player2 .gameScore").text(gameTotal2);
+    $(".roundNum").text(round);
     endGame();
-    $("#gameScore").text(gameTotal);
-    $("#roundNum").text(round);
-  })
+    $("#player2 .gameScore").text(gameTotal2);
+    $("#player1 .gameScore").text(gameTotal1);
+    $(".roundNum").text(round);
+
+
+    // $("#player2 .roundNum").text(round);
+    // $("#player1 .roundNum").text(round);
+  });
+
 });
